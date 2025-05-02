9da891e0d5607c0b7fcb8c5293b4b1659c83292c
haya raham asif <haya.raham-asif@stu.ocsb.ca>
2025-05-02 20:05:26 +0000

    adding header animation

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ let portfolioTitle = document.getElementById("portfolio-title")
 function welcome() {
     document.getElementById("portfolio-title").style.letterSpacing = "15px";
 }
+
+document.addEventListener("scroll", function() {
+    let maxHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight
+    let widthBasedOnScrollPercentage = (window.scrollY/maxHeight)*100 // calulcating what fraction of the webpage the user has scrolled thorugh
+    document.getElementById("progress-bar").style.width = `${widthBasedOnScrollPercentage}%` // sets width of progress bar based on calculated percentage
+})
