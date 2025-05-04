3bf020341c2532c289692d62441ac8a0d62b6535
haya raham asif <haya.raham-asif@stu.ocsb.ca>
2025-05-04 03:38:08 +0000

    made it look a little nicer, subheading animations position responsive now

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-let portfolioTitle = document.getElementById("portfolio-title")
-
-function welcome() {
-    //document.getElementById("portfolio-title").style.letterSpacing = "15px";
-}
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
 
 document.addEventListener("scroll", function() {
     let maxHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight
-    let widthBasedOnScrollPercentage = (window.scrollY/maxHeight)*100 // calulcating what fraction of the webpage the user has scrolled thorugh
-    document.getElementById("progress-bar").style.width = `${widthBasedOnScrollPercentage}%` // sets width of progress bar based on calculated percentage
+    let userScrollPos = window.scrollY
+    let widthBasedOnScrollPercentage = (userScrollPos/maxHeight)*70// calulcating what fraction of the webpage the user has scrolled thorugh
+    document.getElementById("progress-bar").style.width = `${widthBasedOnScrollPercentage}vw` // sets width of progress bar based on calculated percentage
+    aboutMe = document.getElementById("about-me")
+    projects = document.getElementById("projects")
+    skills = document.getElementById("skills")
+    blog = document.getElementById("blog")
+    contact = document.getElementById("contact")
+
+
+    if (userScrollPos >= aboutMe.getBoundingClientRect().top) {
+        console.log("about me in view")
+        aboutMe.querySelector('h1').style.animation = "spaceOut 2s ease-in"
+        aboutMe.querySelector('h1').style.animationFillMode = "forwards"
+    }
+    if (userScrollPos >= projects.getBoundingClientRect().top) {
+        console.log("projects in view")
+        projects.querySelector('h1').style.animation = "spaceOut 2s ease-in"
+        projects.querySelector('h1').style.animationFillMode = "forwards"
+    }
+    if (userScrollPos >= skills.getBoundingClientRect().top) {
+        console.log("skills in view")
+        skills.querySelector('h1').style.animation = "spaceOut 2s ease-in"
+        skills.querySelector('h1').style.animationFillMode = "forwards"
+    }
+    if (userScrollPos >= blog.getBoundingClientRect().top) {
+        console.log("blog in view")
+        blog.querySelector('h1').style.animation = "spaceOut 2s ease-in"
+        blog.querySelector('h1').style.animationFillMode = "forwards"
+    }
+    if (userScrollPos >= contact.getBoundingClientRect().top) {
+        console.log("contact in view")
+        contact.querySelector('h1').style.animation = "spaceOut 2s ease-in"
+        contact.querySelector('h1').style.animationFillMode = "forwards"
+    }
 })
