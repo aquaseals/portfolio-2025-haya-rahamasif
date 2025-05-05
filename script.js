3d287517c21b310b1e3c8c904ea900cb1d47d056
haya raham asif <haya.raham-asif@stu.ocsb.ca>
2025-05-05 02:31:45 +0000

    fixed most animations, next: start adding section specific styling

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.onbeforeunload = function () {
 document.addEventListener("scroll", function() {
     let maxHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight
     let userScrollPos = window.scrollY
-    let widthBasedOnScrollPercentage = (userScrollPos/maxHeight)*70// calulcating what fraction of the webpage the user has scrolled thorugh
+    let widthBasedOnScrollPercentage = (userScrollPos/maxHeight)*100// calulcating what fraction of the webpage the user has scrolled thorugh
     document.getElementById("progress-bar").style.width = `${widthBasedOnScrollPercentage}vw` // sets width of progress bar based on calculated percentage
     aboutMe = document.getElementById("about-me")
     projects = document.getElementById("projects")
@@ -14,28 +14,23 @@ document.addEventListener("scroll", function() {
     contact = document.getElementById("contact")
 
 
-    if (userScrollPos >= aboutMe.getBoundingClientRect().top) {
-        console.log("about me in view")
+    if (userScrollPos >= aboutMe.getBoundingClientRect().top && userScrollPos < projects.getBoundingClientRect().top) {
         aboutMe.querySelector('h1').style.animation = "spaceOut 2s ease-in"
         aboutMe.querySelector('h1').style.animationFillMode = "forwards"
     }
-    if (userScrollPos >= projects.getBoundingClientRect().top) {
-        console.log("projects in view")
+    if (userScrollPos >= projects.getBoundingClientRect().top && userScrollPos < skills.getBoundingClientRect().top) {
         projects.querySelector('h1').style.animation = "spaceOut 2s ease-in"
         projects.querySelector('h1').style.animationFillMode = "forwards"
     }
-    if (userScrollPos >= skills.getBoundingClientRect().top) {
-        console.log("skills in view")
+    if (userScrollPos >= skills.getBoundingClientRect().top && userScrollPos < blog.getBoundingClientRect().top) {
         skills.querySelector('h1').style.animation = "spaceOut 2s ease-in"
         skills.querySelector('h1').style.animationFillMode = "forwards"
     }
-    if (userScrollPos >= blog.getBoundingClientRect().top) {
-        console.log("blog in view")
+    if (userScrollPos >= blog.getBoundingClientRect().top && userScrollPos < contact.getBoundingClientRect().top) {
         blog.querySelector('h1').style.animation = "spaceOut 2s ease-in"
         blog.querySelector('h1').style.animationFillMode = "forwards"
     }
     if (userScrollPos >= contact.getBoundingClientRect().top) {
-        console.log("contact in view")
         contact.querySelector('h1').style.animation = "spaceOut 2s ease-in"
         contact.querySelector('h1').style.animationFillMode = "forwards"
     }
